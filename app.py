a987c5d55e79c7a8d4ee49bcd290ab9c524caad9
Abi-Ash <ashwinimegala374@gmail.com>
2026-05-08 09:02:16 +0530

    port issue

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from db import get_connection
-
+import os
 
 app = Flask(__name__)
 
@@ -183,6 +183,8 @@ def search_employee():
     )
 
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
